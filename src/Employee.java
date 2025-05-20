75ce19744517f0aa8ab643925099a2111f40cb1a
Gosha <bakiyev3005@gmail.com>
2025-05-20 18:13:33 +0300

    initial commit

---
+++ src/Employee.java
@@ -0,0 +1,18 @@
+public class Employee {
+    String firstName;
+    String midleName;
+    String lastName;
+    int salary;
+    byte department;
+    static int id;
+
+    public Employee(String firstName, String midleName, String lastName, int salary, byte department) {
+        this.firstName = firstName;
+        this.midleName = midleName;
+        this.lastName = lastName;
+        this.salary = salary;
+        this.department = department;
+    }
+
+
+}
